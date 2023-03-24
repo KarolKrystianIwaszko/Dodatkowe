de32cbf6efeb90b356a334a5414475b9d7e629fa
KarolKrystianIwaszko <120569511+KarolKrystianIwaszko@users.noreply.github.com>
2023-03-24 18:37:22 +0100

    Update Dot1.3.py

--- Dot1.3.py
+++ Dot1.3.py
@@ -1,22 +1,21 @@
-
 def najbliżej_n(n):
-  a = int(input('podaj a'))
-  b = a-n
-  if b < 0:
-    b=b*-1
-  najmniejsza_różnica = b
-  najbliżej_n = a
-  i=0
-  while i < 4:
-      a = int(input('podaj a'))
-      b = a-n
-      if b < 0:
+    a = int(input('podaj a'))
+    b = a-n
+    if b < 0:
         b=b*-1
-      if b < najmniejsza_różnica:
-        najmniejsza_różnica = b
-        najbliżej_n = a
-      i=i+1
-    
+    najmniejsza_różnica = b
+    najbliżej_n = a
+    i=0
+    while i < 4:
+        a = int(input('podaj a'))
+        b = a-n
+        if b < 0:
+            b=b*-1
+        if b < najmniejsza_różnica:
+            najmniejsza_różnica = b
+            najbliżej_n = a
+        i=i+1
+    return najbliżej_n
     
 n = int(input('podaj n'))
 print('najbliżej na osi liczbowej do',n,'jest',najbliżej_n(n))
