c6db6fe4317222fe5eccb7aea49ece5690224752
KarolKrystianIwaszko <120569511+KarolKrystianIwaszko@users.noreply.github.com>
2023-03-24 18:17:01 +0100

    Create Dot1.0.py

---
+++ Dot1.0.py
@@ -0,0 +1,20 @@
+
+n = int(input('podaj n'))
+
+a = int(input('podaj a'))
+b = a-n
+if b < 0:
+  b=b*-1
+najmniejsza_różnica = b
+najbliżej_n = a
+      
+for i in range(4):
+    a = int(input('podaj a'))
+    b = a-n
+    if b < 0:
+      b=b*-1
+    if b < najmniejsza_różnica:
+      najmniejsza_różnica = b
+      najbliżej_n = a
+      
+print('najbliżej na osi liczbowej do',n,'jest',najbliżej_n,'oddalona o ',najmniejsza_różnica)
