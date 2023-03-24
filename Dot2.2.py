6f45cb919c2876d4b21ebd01822c0871ae4727a3
KarolKrystianIwaszko <120569511+KarolKrystianIwaszko@users.noreply.github.com>
2023-03-24 14:33:25 +0100

    Create Dot2.2.py

---
+++ Dot2.2.py
@@ -0,0 +1,13 @@
+
+n = int(input('Podaj dzielnik'))
+
+a=int(input('Wprowadź daną'))
+minimum = a
+najmniejsza_reszta_z_dzielenia = a%n
+for i in range(4)
+  a=int(input('Wprowadź daną'))
+  if najmniejsza_reszta_z_dzielenia > a%n:
+    minimum=a
+    najmniejsza_reszta_z_dzielenia = a%n
+
+print('Najmniejsza jest reszta z dzielenia',minimum,'przez',n,'równa',minimum%n )
